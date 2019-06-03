72cc751ae25373fa7dee72a3b3e12e9595af6ece
wmhello <871228582@qq.com>
2019-06-04 07:53:07 +0800

    3-1 注册与登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,12 +12,30 @@
             </a>
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <ul class="nav navbar-nav">
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">登录</a></li>
-                <li><a href="#">注册</a></li>
-            </ul>
+            <!-- <ul class="nav navbar-nav">
+            </ul> -->
+      <ul class="navbar-nav navbar-right">
+        <!-- 登录注册链接开始 -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="logout" href="#"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </li>
+        @endguest
+        <!-- 登录注册链接结束 -->
+      </ul>
         </div>
     </div>
 </nav>
